17eb502198a2bc711b0d45b41ed6c58e95815b7c
Adem Srndic <adem.srndic@hotmail.com>
2019-10-17 22:43:08 +0200

    Homework

---
+++ TrippinExerc/TrippinExerc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TrippinExerc
+{
+    class Program
+    {
+        static HttpClient HttpClient = new HttpClient() 
+        { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/(S(npj54vjthjj0d1nvunlzt1sf))/") };
+
+        static async Task Main(string[] args)
+        {
+            var contentOfFile = await File.ReadAllTextAsync("users.json");
+            IEnumerable<User> users = JsonSerializer.Deserialize<IEnumerable<User>>(contentOfFile);
+
+            foreach (var user in users)
+            {
+                var result = await HttpClient.GetAsync("People('" + user.UserName + "')");
+                if (!result.IsSuccessStatusCode)
+                {
+                    var newUser = new StringContent(JsonSerializer.Serialize(new NewUser(user)), Encoding.UTF8, "application/json");
+                    var addedUsers = await HttpClient.PostAsync("People", newUser);
+                    
+                    try
+                    {
+                        addedUsers.EnsureSuccessStatusCode();
+                        Console.WriteLine("User:" + user.UserName + " inserted");
+                    }
+                    catch (Exception e)
+                    {   
+                        Console.WriteLine(e.GetType().ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("User: " + user.UserName + "was found!");
+                }
+            }
+        }
+    }
+}
---
+++ TrippinExerc/TrippinExerc/User.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TrippinExerc
+{
+    public class User
+    {
+
+        [JsonPropertyName("UserName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("FirstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("LastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("Email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("Address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("CityName")]
+        public string CityName { get; set; }
+
+        [JsonPropertyName("Country")]
+        public string Country { get; set; }
+
+    }
+
+    public class NewUser
+    {
+
+        [JsonPropertyName("UserName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("FirstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("LastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("Emails")]
+        public IEnumerable<string> Emails { get; set; }
+
+        [JsonPropertyName("AddressInfo")]
+        public IEnumerable<AddressInfo> AddressInfo { get; set; }
+
+        public NewUser(User user)
+        {
+            this.UserName = user.UserName;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+            this.Emails = new List<string>() { user.Email };
+
+            this.AddressInfo = new List<AddressInfo>(){ new AddressInfo(user.Address, new City(user.CityName, user.Country, "")) };
+        }
+
+    }
+
+    public class AddressInfo
+    {
+
+        [JsonPropertyName("Address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("City")]
+        public City City { get; set; }
+
+        public AddressInfo(string Address, City City)
+        {
+            this.Address = Address;
+            this.City = City;
+        }
+
+    }
+
+    public class City
+    {
+
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("CountryRegion")]
+        public string CountryRegion { get; set; }
+
+        [JsonPropertyName("Region")]
+        public string Region { get; set; }
+
+        public City(string Name, string CountryRegion, string Region)
+        {
+            this.Name = Name;
+            this.CountryRegion = CountryRegion;
+            this.Region = Region;
+        }
+
+    }
+}
